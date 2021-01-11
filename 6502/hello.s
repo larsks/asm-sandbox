7d2687557986fd6f5148c8c74bb9396f2d765e05
Lars Kellogg-Stedman <lars@oddbit.com>
2021-01-11 08:21:57 -0500

    code cleanup

--- 6502/hello.s
+++ 6502/hello.s
@@ -1,20 +1,21 @@
-; goodbyeworld.s for C= 8-bit machines, ca65 assembler format.
+; helloworld.s for C= 8-bit machines, 64tass assembler format.
 ; String printing limited to strings of 256 characters or less.
 
 * = $c000
  
 a_cr	= $0d		; Carriage return.
 bsout	= $ffd2		; C64 KERNEL ROM, output a character to current device.
-			; use $fded for Apple 2
-	ldx #0		; Starting index 0 in X register.
+
+main:
+	ldx #0			; Starting index 0 in X register.
 printnext:
-	lda text,x	; Get character from string.
-	beq done	; If we read a 0 we're done.
-	jsr bsout	; Output character. 
-	inx		; Increment index to next character.
+	lda text,x		; Get character from string.
+	beq done		; If we read a 0 we're done.
+	jsr bsout		; Output character. 
+	inx				; Increment index to next character.
 	bne printnext	; Repeat if index doesn't overflow to 0.
 done:
-	rts		; Return from subroutine.
+	rts				; Return from subroutine.
  
 text:
 	.text	"Hello world!", a_cr, 0
