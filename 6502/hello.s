9e404cdc9189944df9e330a45dbd8637133820b8
Lars Kellogg-Stedman <lars@oddbit.com>
2021-01-12 09:47:54 -0500

    retab using spaces

--- 6502/hello.s
+++ 6502/hello.s
@@ -1,20 +1,20 @@
-a_cr	= $0d		; Carriage return.
-CHROUT	= $ffd2		; C64 KERNEL ROM, output a character to current device.
+a_cr    = $0d       ; Carriage return.
+CHROUT  = $ffd2     ; C64 KERNEL ROM, output a character to current device.
 
 .code
 .include "bootstrap.s"
 
 main:
-	ldx #0			; Starting index 0 in X register.
+    ldx #0          ; Starting index 0 in X register.
 printnext:
-	lda text,x		; Get character from string.
-	beq done		; If we read a 0 we're done.
-	jsr CHROUT		; Output character. 
-	inx				; Increment index to next character.
-	bne printnext	; Repeat if index doesn't overflow to 0.
+    lda text,x      ; Get character from string.
+    beq done        ; If we read a 0 we're done.
+    jsr CHROUT      ; Output character. 
+    inx             ; Increment index to next character.
+    bne printnext   ; Repeat if index doesn't overflow to 0.
 done:
-	rts				; Return from subroutine.
+    rts             ; Return from subroutine.
  
 .data
 text:
-	.asciiz	"hello world!"
+    .asciiz "hello world!"
